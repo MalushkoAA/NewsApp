6ec9b046909a70b2d6f92eb420b1373a63c2cbba
Aleksey Malushko <alekseymalushko@gmail.com>
2022-03-18 09:43:08 +0300

    Добавил api service и api factory. Проверил ответ сервера.

--- app/src/main/java/com/example/newsapp/data/network/ApiFactory.kt
+++ app/src/main/java/com/example/newsapp/data/network/ApiFactory.kt
@@ -1,4 +1,28 @@
 package com.example.newsapp.data.network
 
-class ApiFactory {
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ApiFactory {
+
+    var interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+    var client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+    private const val BASE_URL = "https://newsapi.org/v2/"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .client(client)
+        .build()
+
+    val apiService = retrofit.create(ApiService::class.java)
 }
--- app/src/main/java/com/example/newsapp/data/network/models/NewsItemDto.kt
+++ app/src/main/java/com/example/newsapp/data/network/models/NewsItemDto.kt
@@ -1,10 +1,14 @@
 package com.example.newsapp.data.network.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class NewsItemDto(
-    val content: String,
-    val description: String,
-    val publishedAt: String,
-    val title: String,
-    val url: String,
-    val urlToImage: String
-)
+    val content: String? = null,
+    val description: String? = null,
+    val publishedAt: String? = null,
+    val title: String? = null,
+    val url: String? = null,
+    val urlToImage: String? = null
+) : Parcelable
