47310da82affd3b28899c2a94627e30ab23253a5
Aleksey Malushko <alekseymalushko@gmail.com>
2022-08-14 12:22:12 +0300

    Add Dagger2 di

--- app/src/main/java/com/example/newsapp/ui/App.kt
+++ app/src/main/java/com/example/newsapp/ui/App.kt
@@ -1,11 +1,10 @@
 package com.example.newsapp.ui
 
 import android.app.Application
-import com.example.newsapp.di.AppComponent
 import com.example.newsapp.di.DaggerAppComponent
 
 class App:Application() {
-    val appComponent:AppComponent by lazy {
+    val appComponent by lazy {
         DaggerAppComponent.factory().create(this)
     }
 }
--- app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/viewmodels/NewsViewModel.kt
@@ -1,11 +1,8 @@
 package com.example.newsapp.ui.viewmodels
 
-import android.app.Application
 import android.content.Context
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.newsapp.data.repo.NewsRepoImpl
 import com.example.newsapp.domain.usecases.GetNewsItemUseCase
 import com.example.newsapp.domain.usecases.GetNewsItemsListUseCase
 import com.example.newsapp.domain.usecases.LoadDataUseCase
@@ -14,23 +11,23 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NewsViewModel @Inject constructor(
-    private val getNewsItemsListUseCase:GetNewsItemsListUseCase,
-    private val loadDataUseCase:LoadDataUseCase,
-    private val getNewsItemUseCase:GetNewsItemUseCase,
-    private val openNewsInSourceUseCase:OpenNewsInSourceUseCase
+    private val getNewsItemsListUseCase: GetNewsItemsListUseCase,
+    private val loadDataUseCase: LoadDataUseCase,
+    private val getNewsItemUseCase: GetNewsItemUseCase,
+    private val openNewsInSourceUseCase: OpenNewsInSourceUseCase
 ) : ViewModel() {
-
+    
     fun openNews(context: Context, url: String) = openNewsInSourceUseCase(context, url)
-
+    
     fun getNewsDetail(url: String) = getNewsItemUseCase(url)
-
+    
     val newsItemsList = getNewsItemsListUseCase()
-
+    
     init {
         viewModelScope.launch {
-            loadDataUseCase()
+                loadDataUseCase()
         }
     }
-
-
+    
+    
 }
